72fd1508f9696b6075f36d59e3b5ff0da93eb49b
Camilo Medina <camedi445@gmail.com>
2020-10-28 01:25:42 -0500

    Dependencies added

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+buildscript {
+    repositories {
+        gradlePluginPortal()
+        jcenter()
+        google()
+        mavenCentral()
+    }
+    val toolsGradleVersion = "4.2.0-alpha14"
+    val kotlinVersion = "1.4.10"
+    val sqlDelightVersion: String by project
+
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+        classpath("com.android.tools.build:gradle:$toolsGradleVersion")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+        classpath("org.jetbrains.kotlin:kotlin-serialization:$kotlinVersion")
+        classpath("com.squareup.sqldelight:gradle-plugin:$sqlDelightVersion")
+    }
+}
+group = "com.cmedina.sports"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
